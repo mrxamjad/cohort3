82e79d7960e005e5058c7c892b3718be4ce65224
amjad-raptee <amjadali@raptee.com>
2024-08-25 12:33:28 +0530

    Nav Bar completed for Trevello html project

---
+++ node-js/week-4/first/Readme.md
@@ -0,0 +1,1 @@
+## nmp init -y
---
+++ node-js/week-4/first/index.js
@@ -0,0 +1,8 @@
+const path=require('path');
+
+console.log(path.resolve(__dirname, 'index.mjs'));
+function sum(a, b) {                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+  return a + b;
+}
+
+console.log(sum(1,3))
